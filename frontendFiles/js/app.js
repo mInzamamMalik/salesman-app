2b255ce8f7d8aa2d733c030280d4df1442dbbea6
malikasinger1@gmail.com <malikasinger1@gmail.com>
2016-01-28 17:26:15 +0500

    frontend files updating

--- frontendFiles/js/app.js
+++ frontendFiles/js/app.js
@@ -5,15 +5,19 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+
+.config(function($urlRouterProvider , $stateProvider){
+
+  $urlRouterProvider.otherwise("/");
+
+  $stateProvider
+      .state("signup",{
+        url : "/signup",
+        templateUrl : ".views/signup.html"
+      });
+
+
+
+});
+
+
