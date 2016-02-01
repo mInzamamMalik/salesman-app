89e73c591faf5deb9eb14bd4af5d2b0650dfb659
malikasinger1@gmail.com <malikasinger1@gmail.com>
2016-02-01 21:10:33 +0500

    improving ui:  loding and alert is applied to attract user

--- frontendFiles/views/signup/signup.js
+++ frontendFiles/views/signup/signup.js
@@ -1,86 +1,81 @@
 (function() {
     angular.module("signup", [])
 
-        .controller("signupController",function ($scope, $http, $state, $ionicLoading) {
-                $scope.userObject = {};
+        .controller("signupController",function ($scope, $http, $state, $ionicLoading, $ionicPopup) {
+            $scope.userObject = {};
 
-                $scope.signup = function(){
-
-                    $scope.showLoding();
+            $scope.signup = function () {
 
 
 
-                    /*interface patternOfUserObject {
-
-                            email       :    string,
-                            password    :    string,
-                            firstName   :    string,      //this is a interface which is must required from front end
-                            lastName    :    string,
-                            companyName :    string
-
-                    }*/
+                $scope.showLoding();
 
 
+                /*interface patternOfUserObject {
 
-                    $http({
-                        method : "post",
-                        url : "/v1/signup",
-                        data : {
-                            // this values are required by api if any one is missing there will be problem
-                            email : $scope.userObject.email,
-                            password : $scope.userObject.password,
-                            firstName : $scope.userObject.firstName,
-                            lastName : $scope.userObject.lastName,
-                            companyName : $scope.userObject.companyName
+                 email       :    string,
+                 password    :    string,
+                 firstName   :    string,      //this is a interface which is must required from front end
+                 lastName    :    string,
+                 companyName :    string
 
+                 }*/
+
+
+                $http({
+                    method: "post",
+                    url: "/v1/signup",
+                    data: {
+                        // this values are required by api if any one is missing there will be problem
+                        email: $scope.userObject.email,
+                        password: $scope.userObject.password,
+                        firstName: $scope.userObject.firstName,
+                        lastName: $scope.userObject.lastName,
+                        companyName: $scope.userObject.companyName
+
+                    }
+                }).then(
+                    function (response) {
+                        console.log("res: ", response.data);
+
+                        $scope.hideLoding();
+                        ////////////////////
+
+                        if (response.data.signup) {
+
+                            $scope.showAlert("Congratulation !!","signed up successfully, please login to continue...");
+                            $state.go("login");
+                        } else {
+                            $scope.showAlert("Login Failed !!",response.data.message);
                         }
-                    }).then(
-                        function(response){
-                            console.log("res: ", response.data);
-
-                            $scope.hideLoding();
-
-                            if(response.data.signup){
-                                $state.go("login");
-                            }else{
-                                alert(response.data.message);
-                            }
-                        },
-                        function(error){
-                            console.log("error: ", error);
-                        }
-                    );
-                };
-
-
+                    },
+                    function (error) {
+                        console.log("error: ", error);
+                    }
+                );
+            };
 
 
 ////////////////////loding code startted ///////////////
-            $scope.showLoding = function() {
+            $scope.showLoding = function () {
                 $ionicLoading.show({
                     template: 'Signing Up...'
                 });
             };
-            $scope.hideLoding = function(){
+            $scope.hideLoding = function () {
                 $ionicLoading.hide();
             };
-
-            $scope.showAlert = function(title,template) {
-                var alertPopup = $ionicPopup.alert({
+            $scope.showAlert = function(title,template){
+                $ionicPopup.alert({
                     title: title,
                     template: template
                 });
+            };
+
 ////////////////////loding code ended /////////////////
 
 
-
-
-            }
-        );
-
-
-
-
+        })
 
 
 
