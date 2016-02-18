f868eead1dae0953efdbad6bf298f36d25a6aaf1
malikasinger1@gmail.com <malikasinger1@gmail.com>
2016-02-18 21:00:48 +0500

    completed - flaging on firebase for notification is

---
+++ DBrepo/notificationMethods.js
@@ -0,0 +1,30 @@
+var mongoose = require("mongoose"); //mongodb driver
+var Firebase = require("firebase");
+var q = require("q"); //to return deferred.promise from function
+var ref = new Firebase("https://sales-man-app.firebaseio.com/");
+var notification = {
+    incrementOne: function (companyUid) {
+        var notificationcount = null;
+        var notificationNode = ref.child(companyUid.toString() + "/notificationCount");
+        //firstly get current notification count by ref.once function    
+        notificationNode.once("value", function (snapshot) {
+            // console.log("this data is received by ref.once firebase in json form: ", snapshot.val());
+            notificationcount = snapshot.val();
+            //console.log(notificationcount , typeof notificationcount);            
+            //increment one
+            //null from firebase on no previous data is handled by if statment
+            if (notificationcount)
+                notificationcount++;
+            else
+                notificationcount = 1;
+            //then save this notification value to firabase
+            notificationNode.set(notificationcount);
+        });
+    },
+    clear: function (companyUid) {
+        var notificationNode = ref.child(companyUid.toString() + "/notificationCount");
+        notificationNode.set(0);
+    }
+};
+exports.notification = notification;
+notification.incrementOne(123);
---
+++ DBrepo/notificationMethods.ts
@@ -0,0 +1,53 @@
+var mongoose = require("mongoose"); //mongodb driver
+var Firebase = require("firebase");
+var q = require("q"); //to return deferred.promise from function
+
+var ref = new Firebase("https://sales-man-app.firebaseio.com/");
+
+
+let notification = {
+
+
+
+
+
+
+    incrementOne: (companyUid) => {
+
+        let notificationcount = null;
+
+        let notificationNode = ref.child(companyUid.toString() + "/notificationCount");
+
+        //firstly get current notification count by ref.once function    
+        notificationNode.once("value", function(snapshot) {
+            // console.log("this data is received by ref.once firebase in json form: ", snapshot.val());
+
+            notificationcount = snapshot.val();
+            
+            //console.log(notificationcount , typeof notificationcount);            
+
+            //increment one
+            //null from firebase on no previous data is handled by if statment
+            if (notificationcount) notificationcount++;
+            else notificationcount = 1; 
+
+            //then save this notification value to firabase
+            notificationNode.set(notificationcount);
+        })
+    },
+
+
+    clear: (companyUid) => {
+        let notificationNode = ref.child(companyUid.toString() + "/notificationCount");
+        notificationNode.set(0);
+    }
+
+}
+
+
+
+
+notification.incrementOne(123);
+
+
+export { notification }
